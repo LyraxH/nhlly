7241be569b3621a43a43b08d04c3bd8bc5a6ff12
LyraxH <71412637+LyraxH@users.noreply.github.com>
2026-01-18 23:28:22 +1300

    added roster look up. basics

--- nhlly.py
+++ nhlly.py
@@ -20,6 +20,7 @@ def get_standings() -> dict | None:
             'points': team['points'],
             'wins': team['wins'],
             'losses': team['losses'],
+            'otl': team['otLosses'],
             'conf': team['conferenceName'],
             'conf_abbrev': team['conferenceAbbrev'],
             'div': team['divisionName'],
@@ -31,7 +32,6 @@ def get_standings() -> dict | None:
 
 def standings_tui():
     sort_by = "points"
-    
     while True:
         subprocess.run(["clear"])
         standings = get_standings()
@@ -41,37 +41,87 @@ def standings_tui():
             reverse = (sort_by != "name")
         )
         print(f"--- Standings --- Sort By: {sort_by}")
-        print(f"{"Division":<12} {"Team":<22} {"Points":<8} {"W":<4} {"L":<4}")
-        print("-" * 60)
+        print(f"{"Division":<12} {"Team":<22} {"Points":<8} {"W":<4} {"L":<4} {"OTL":<4}")
+        print("-" * 69)
         for name, stats in sorted_list:
-            print(f"{stats['div']:<12} {name:<22} {stats['points']:<8} {stats['wins']:<4} {stats['losses']:<4}")
-        print("\n Sort By: (p)oints, (d)ivision, (t)eam, (q)uit")
-        choice = input("\nChoose an option: ").lower()
+            print(f"{stats['div']:<12} {name:<22} {stats['points']:<8} {stats['wins']:<4} {stats['losses']:<4} {stats['otl']:<4}")
+        choice = input("Sort By: (p)oints, (d)ivision, (t)eam, (q)uit").lower()
         match choice:
             case 'p':
                 sort_by = "points"
             case 'd':
                 sort_by = "div"
             case 't':
-                sort_by = "team"
+                sort_by = "name"
             case 'q':
                 break
             case _:
                 print("Invalid input")
+
+def get_skaters(team_abbrev):
+    data = get_data(f"club-stats/{team_abbrev}/now")
+    if not data or 'skaters' not in data:
+        return None
+    return {
+        player['lastName']['default']: {
+            'first_name': player['firstName']['default'],
+            'player_id': player['playerId'],
+            'games': player['gamesPlayed'],
+            'goals': player['goals'],
+            'assists': player['assists'],
+            'points': player['points'],
+            '+/-': player['plusMinus'],
+            'penalty_minutes': player['penaltyMinutes'],
+            'toi': player['avgTimeOnIcePerGame'],
+            'faceoffWinPctg': player['faceoffWinPctg']
+        }
+        for player in data.get('skaters', [])
+    }
+
+def get_goalies(team_abbrev):
+    data = get_data(f"club-stats/{team_abbrev}/now")
+    if not data or 'goalies' not in data:
+        return None
+    return {
+        player['lastName']['default']: {
+            'first_name': player['firstName']['default'],
+            'player_id': player['playerId'],
+            'games': player['gamesPlayed'],
+            'wins': player['wins'],
+            'losses': player['losses'],
+            'otl': player['overtimeLosses'],
+            'shutouts': player['shutouts'],
+            'sv': player['savePercentage'],
+            'gaa': player['goalsAgainstAverage']
+        }
+        for player in data.get('goalies', [])
+    }
+
+def team_tui():
+    team_abbrev = input("What team: ").upper()
+    skaters = get_skaters(team_abbrev)
+    goalies = get_goalies(team_abbrev)
+    subprocess.run(["clear"])
+    sorted_skaters = sorted(skaters.items(), key=lambda x: x[1]['first_name'], reverse=True)
+    for name, stats in sorted_skaters:
+        print(f"{stats['first_name']:<12} [{name}]: {stats['games']}")
+    sorted_goalies = sorted(goalies.items(), key=lambda x: x[1]['first_name'], reverse=True)
+    for name, stats in sorted_goalies:
+        print(f"{stats['first_name']:<12} [{name}]: {stats['games']}")
 
 def main():
     """
     Main function for nhlly
     """
     while True:
-        user_input = input("(s)tandings (g)ames (p)layoffs (q)uit \n what do: ").lower()
+        user_input = input("(s)tandings (g)ames (t)eam (q)uit \n what do: ").lower()
         match user_input:
             case 's':
                 standings_tui()
             case 'g':
                 get_games()
-            case 'p':
-                get_players()
+            case 't':
+                team_tui()
             case 'q':
                 break
             case _:
