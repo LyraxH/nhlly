6bf934a93d227cf2fe336917374836616b825027
LyraxH <71412637+LyraxH@users.noreply.github.com>
2026-01-20 20:57:42 +1300

    fixed nhlly_db to new db updates

--- nhlly_db.py
+++ nhlly_db.py
@@ -2,30 +2,26 @@ import sqlite3
 
 RESET = "\033[0m"
 
-def get_colors(tricode, color_num):
+def get_colors(code, color_num):
     """
     Fetches colors for a given team from attached SQL database
     """
     path = "nhlly_data.db"
     with sqlite3.connect(path) as conn:
         cursor = conn.cursor()
-        cursor.execute("SELECT color_primary, color_secondary, color_accent, neutral_light, neutral_dark FROM colors WHERE tricode = ?", (tricode,))
+        cursor.execute("SELECT light, dark, accent FROM colors WHERE code = ?", (code,))
         row = cursor.fetchone()
         if row:
-            primary, secondary, accent, neutral_light, neutral_dark = row
+            light, dark, accent= row
             match color_num:
                 case 1:
-                    return primary
+                    return light
                 case 2:
-                    return primary, secondary
+                    return light, dark
                 case 3:
-                    return primary, secondary, accent
-                case 4:
-                    return primary, secondary, accent, neutral_light
-                case 5:
-                    return primary, secondary, accent, neutral_light, neutral_dark
+                    return light, dark, accent
         else:
-            print(f"Error: No colors found for {tricode}")
+            print(f"Error: No colors found for {code}")
             return None
 
 def colorize(hex):
